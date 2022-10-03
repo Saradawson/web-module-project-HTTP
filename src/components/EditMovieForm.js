f6a63ad84a05c0ad6e6d8d36001c56c92c50fe62
unknown <saradawsone@gmail.com>
2022-10-03 14:36:16 -0600

    editing movie form updates local state

--- src/components/EditMovieForm.js
+++ src/components/EditMovieForm.js
@@ -38,7 +38,7 @@ const EditMovieForm = (props) => {
         e.preventDefault();
         axios.put(`http://localhost:9000/api/movies/${id}`, movie)
             .then(res=>{
-                setMovies(res.data);
+                props.setMovies(res.data);
                 push(`/movies/${movie.id}`);
 			})
 			.catch(err=>{
